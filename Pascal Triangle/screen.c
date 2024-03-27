26651925ff6746dc2ced591da53c7a32e6230bcd
e2301469 <e2301469@WA2029-13-OLD>
2024-03-27 15:08:23 +0200

    Add project files.

---
+++ Pascal Triangle/screen.c
@@ -0,0 +1,22 @@
+#include "screen.h"
+#include <stdio.h>
+
+void setFGcolor(int color) {
+	printf("\x1B[1;%dm", color);
+	fflush(stdout);
+}
+
+void clearScreen(void) {
+	printf("\x1B[2J");
+	fflush(stdout);
+}
+
+void gotoXY(int row, int col) {
+	printf("\x1B[%d;%dH", row, col);
+	fflush(stdout);
+}
+
+void resetColor() {
+	printf("\x1B[0m");
+	fflush(stdout);
+}
---
+++ Pascal Triangle/screen.h
@@ -0,0 +1,11 @@
+
+enum COLORS {
+	BLACK=30,
+	RED,
+	GREEN,
+	YELLOW,
+	BLUE,
+	MAGENTA,
+	CYAN,
+	WHITE
+};
